a1f71330a19fa62f66a39324a9ff6ab79d717f2e
Toby Burnett <tburnett@uw.edu>
2019-10-01 10:39:36 -0700

    update setup.sh to add to comment

--- setup.sh
+++ setup.sh
@@ -13,6 +13,8 @@ export PYTHONPATH=$here/code:$PYTHONPATH
 
 # to start a jupyter lab server - note the port
 # to connect from a remote computer, run ssh -N -f -L localhost:<localport>:localhost:8890 <userid>@rhel6-64x.slac.stanford.edu
+# and, on the remote computer, ssh -N -f -L localhost:8888:localhost:8890 burnett@rhel6-64X.slac.stanford.edu, where "X" is the
+# particular host. 
 alias jupyterlab='jupyter lab --notebook-dir=$here --port=8890 --no-browser'
 
 
